84c8b7c200ebe06a948b670c03e1de6038259033
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-29 21:29:52 +0530

    learnig bitwise operators

---
+++ operators.js
@@ -0,0 +1,16 @@
+// bitwise operator
+
+console.log(5&2)
+console.log(5>>1);
+console.log(5<<2);
+console.log(5|2);
+console.log(3^3);
+
+
+
+// string concatination
+console.log("rohan"+" kumar");
+console.log(1-"2")
+console.log("3"+3)
+console.log();
+
