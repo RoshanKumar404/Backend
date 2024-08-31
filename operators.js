9b93b46a938ceda37f5655a520022898786c0937
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-31 19:15:02 +0530

    started knowing about coersion

--- operators.js
+++ operators.js
@@ -1,18 +1,34 @@
-// bitwise operator
+// // bitwise operator
 
-console.log(5&2)
-console.log(5>>1);
-console.log(5<<2);
-console.log(5|2);
-console.log(3^3);
+// console.log(5&2)
+// console.log(5>>1);
+// console.log(5<<2);
+// console.log(5|2);
+// console.log(3^3);
 
 
 
-// string concatination
-console.log("rohan"+" kumar");
-console.log(1-"2")
-console.log("3"+3)
-console.log(1+"2");
-//ternary operator
+// // string concatination
+// console.log("rohan"+" kumar");
+// console.log(1-"2")
+// console.log("3"+3)
+// console.log(1+"2");
+// //ternary operator
 
-3>5?console.log(true):console.log(false)
+// 3>5?console.log(true):console.log(false)
+
+
+// let val=10;
+// switch(val){
+
+//     case '10':
+//         console.log("yes");
+//         break;
+//         default:
+//             console.log("no");
+//             break;
+        
+// }
+
+let x= console.log;
+console.log(x);
