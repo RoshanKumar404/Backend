aafbf257173dfea9d6b35225bf80ea54891edef7
RoshanKumar404 <roshankuar951@gmail.com>
2024-09-01 22:32:27 +0530

    learned basic express things likek req response next , error handling

--- express.js
+++ express.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
-
+app.use(function(req,res ,next){
+  console.log("ku6 to ho rha hai");
+  next();
+})
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send("Hello World to ho rha hai")
+})
+app.use(function(req,res ,next){
+  console.log("ku6 to ho rha hai profile load hone se pahle");
+  // return next(new Error("something went wrong at line no. 12"))
+   next();
+})
+
+app.get('/profile', function(req,res,next){
+  res.send("this is profile")
+})
+app.get('/profile/home', function(req,res,next){
+  //res.send("this is home someting changed profile ")
+  return next(new Error("something went wrong at line number 18"))
+
+})
+
+app.use(function(err,req,res,next){
+  console.error(err.stack);
+  res.status(500).send('something broked')
 })
 
 app.listen(3000)
